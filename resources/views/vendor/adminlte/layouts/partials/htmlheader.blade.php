4cecd96b867a3d82bc65348fe5a16e828330600d
Bry-Esc <yusar0000@gmail.com>
2023-11-26 16:35:30 +0800

    fixed stytling bugs

--- resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
@@ -1,15 +1,14 @@
-
 <head>
     <meta charset="UTF-8">
     <title> PCU SCHD </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <link href="{{ asset('/css/all.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('/plugins/select2/select2.css') }}" rel="stylesheet" type="text/css" />
     <script src="https://js.pusher.com/4.4/pusher.min.js"></script>
     <link rel="stylesheet" href="{{ asset ('plugins/toastr/toastr.css')}}">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
--- resources/views/vendor/adminlte/super_layout/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte/super_layout/partials/htmlheader.blade.php
@@ -5,6 +5,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ asset('/css/all.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/plugins/select2/select2.css') }}" rel="stylesheet" type="text/css" />
+    <script src="https://js.pusher.com/4.4/pusher.min.js"></script>
+    <link rel="stylesheet" href="{{ asset ('plugins/toastr/toastr.css')}}">
     
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> --}}
